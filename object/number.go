46218e8068e7c8b2b118bd976a73155f018f6b73
nbvghost <nbvghost@gmail.com>
2025-01-09 23:29:26 +0800

    number.go add Decimal method

--- object/number.go
+++ object/number.go
@@ -13,6 +13,14 @@ import (
 	"strconv"
 )
 
+// Decimal float64 保留小数点后位数
+// value float64 浮点数
+// prec int 需保留小数点后的位数
+func Decimal(value float64, prec int) float64 {
+	value, _ = strconv.ParseFloat(strconv.FormatFloat(value, 'f', prec, 64), 64)
+	return value
+}
+
 // ParseFloat /*转化为float64*/
 func ParseFloat(value interface{}) float64 {
 	if value == nil {
